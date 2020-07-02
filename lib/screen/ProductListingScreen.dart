6b244ec4b70cfcea9f5ffbc65bf297dd59cc79d4
Prathamesh Mestry <prathameshmestry1992@gmail.com>
2020-07-02 17:19:28 +0530

    changes in font style in Text in product listing.

--- lib/screen/ProductListingScreen.dart
+++ lib/screen/ProductListingScreen.dart
@@ -23,33 +23,47 @@ class ProductListingScreen extends StatelessWidget {
         ],
       ),
       body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: 1,
+          padding: const EdgeInsets.all(10),
+          itemCount: 3,
           itemBuilder: (BuildContext context, int index) {
             return Container(
-              color: Colors.grey,
+              margin: const EdgeInsets.symmetric(vertical: 5),
               padding: const EdgeInsets.all(10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   new Image.network(
-                    'https://simpleicon.com/wp-content/uploads/table-256x256.png',
-                    width: 60,
-                    height: 60,
+                    'https://www.ulcdn.net/images/products/157781/product/Wesley_Dalla_6_Seater_Dining_Table_Set_Grey_LP.jpg?1508904699',
+                    width: 70,
+                    height: 70,
                   ),
                   new Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      new Text('Stylish Modern Dining Tables'),
-                      new Text('Aron Table Center'),
+                      new Text(
+                        'Stylish Modern Dining Tables',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      new Text(
+                        'Aron Table Center',
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal, color: Colors.grey),
+                      ),
                       new Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          new Text('Rs. 24000'),
+                          new Text(
+                            'Rs. 24000',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red,
+                                fontSize: 15),
+                          ),
                           new Image.network(
                             'https://simpleicon.com/wp-content/uploads/table-256x256.png',
-                            height: 20,
+                            height: 18,
                           ),
                         ],
                       )
