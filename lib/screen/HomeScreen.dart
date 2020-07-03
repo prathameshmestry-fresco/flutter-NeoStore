45dd39436ec57117afb7e784827ed1cf9655a4c8
Prathamesh Mestry <prathameshmestry1992@gmail.com>
2020-07-03 19:04:27 +0530

    add package for slider .

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,22 +1,36 @@
-import 'package:NeoSTORE/screen/ProductListingScreen.dart';
-
-import '../widgets/ProductCategoryTile.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 import '../model/ProductCategory.dart';
 import 'package:flutter/material.dart';
+import '../widgets/ProductCategoryTile.dart';
+import '../screen/ProductListingScreen.dart';
 
 void main() {
   runApp(HomeScreenPage());
 }
 
-class HomeScreenPage extends StatelessWidget {
+class HomeScreenPage extends StatefulWidget {
+  @override
+  _HomeScreenPageState createState() => _HomeScreenPageState();
+}
+
+class _HomeScreenPageState extends State<HomeScreenPage> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(home: new HomeScreen());
   }
 }
 
+// ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
+  int _currentIndex = 0;
+
+  List<String> imgList = [
+    'https://www.itl.cat/pngfile/big/202-2025052_wallpaper-design-for-living-room-home-decoration-ideas.jpg',
+    'https://cdn11.bigcommerce.com/s-pkla4xn3/images/stencil/1280x1280/products/29676/277689/Customized-simple-3D-wallpaper-geometric-diamond-murals-for-living-room-bedroom-sofa-background-wall-home-decoration__86750.1567751324.jpg?c=2?imbypass=on',
+    'https://ae01.alicdn.com/kf/HTB1zsShXo_rK1Rjy0Fcq6zEvVXah.jpg',
+    'https://sc02.alicdn.com/kf/HTB1MuxBXdjvK1RjSspiq6AEqXXa7/234456910/HTB1MuxBXdjvK1RjSspiq6AEqXXa7.jpg'
+  ];
+
   final List<ProductCategory> categorList = [
     ProductCategory(
         id: 1,
@@ -52,25 +66,32 @@ class HomeScreen extends StatelessWidget {
             IconButton(icon: Icon(Icons.search), onPressed: () {})
           ],
         ),
-        body: GridView.builder(
-          padding: const EdgeInsets.all(10),
-          itemCount: categorList.length,
-          itemBuilder: (context, i) => new InkResponse(
-            enableFeedback: true,
-            child: ProductCategoryTile(categorList[i].id, categorList[i].title,
-                categorList[i].imageUrl),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ProductListingScreen()));
-            },
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new CarouselSlider(
+                  height: 400.0,
+                  autoPlay: true,
+                  reverse: false,
+                  scrollDirection: Axis.horizontal,
+                  initialPage: 0,
+                  items: imgList.map((imageLink) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            child: Image.network(
+                              imageLink,
+                              fit: BoxFit.cover,
+                            ));
+                      },
+                    );
+                  }).toList()),
+            ],
           ),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10),
         ));
   }
 }
